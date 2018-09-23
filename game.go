6182391f350ad2f3e8f1575b0b40057a1895085c
robbagott <robbagott@gmail.com>
2018-09-22 21:53:04 -0500

    Fully implemented move reads

--- game.go
+++ game.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 )
+
+var moveExp = *regexp.MustCompile("(?P<file1>[a-h])(?P<rank1>[1-8])(?P<file2>[a-h])(?P<rank2>[1-8])(?P<promotionPiece>[bnrq])?")
 
 // Start initiates a chess game with RobChess.
 func Start() {
@@ -25,40 +29,65 @@ func readMove() Move {
 		fmt.Println("What a nice move!")
 		return move
 	}
-	fmt.Println("The move entered could not be understood. Please enter a move in algrebraic chess notation.")
+	fmt.Println("The move entered could not be understood. Please enter a move in long algrebraic chess notation.")
 	return readMove()
 }
 
 func scanMove() string {
-	input := ""
-	fmt.Scanln(&input)
-	return input
+	moveStr := ""
+	fmt.Scan(&moveStr)
+	return moveStr
 }
 
 func algebraicToMove(algMove string) (Move, bool) {
-	// TODO implement
+	algMove = strings.ToLower(algMove)
+	match := moveExp.FindStringSubmatch(algMove)
+	fmt.Println(match)
+	// match will include the full string enclosing the submatches as the first element. Therefore, we look
+	// for a minimum of length 5.
+	if len(match) >= 5 {
+		// Convert each match subgroup to a rune array. The first element in the rune array is the file/rank.
+		// 'a' to '0' is a difference of 48 in html codes.
+		file1Rune := []rune(match[1])[0]
+		file1 := int(file1Rune - 48 - 48)
+		rank1Rune := []rune(match[2])[0]
+		rank1 := int(rank1Rune - 48)
+		file2Rune := []rune(match[3])[0]
+		file2 := int(file2Rune - 48 - 48)
+		rank2Rune := []rune(match[4])[0]
+		rank2 := int(rank2Rune - 48)
+
+		// Optional promotion component
+		var promoPiece string
+		if len(match) == 6 {
+			promoPiece = match[5]
+		}
+
+		move, ok := NewMove(file1, rank1, file2, rank2, promoPiece)
+		return move, ok
+	}
 	return Move{}, false
 }
 
 // Move represents a move on the chess board. It encompasses a piece, the old square and the new square.
 type Move struct {
-	piece        GamePiece
-	oRank, oFile int
-	nRank, nFile int
+	oFile, oRank int
+	nFile, nRank int
+	promoPiece   string
 }
 
 // NewMove creates and initializes a new Move object.
-func NewMove(piece GamePiece, oRank, oFile, nRank, nFile int) (Move, bool) {
-
+func NewMove(oFile, oRank, nFile, nRank int, promoPiece string) (Move, bool) {
+	fmt.Println(oFile, oRank, nFile, nRank, promoPiece)
 	if oRank < 0 || oFile > 7 ||
 		nRank < 0 || nFile > 7 {
 		return Move{}, false
 	}
 	move := Move{
-		piece,
-		oRank,
 		oFile,
+		oRank,
+		nFile,
 		nRank,
-		nFile}
+		promoPiece}
 	return move, true
 }
